29a8e3b0b1ce319ddbaac93b13be0cde8f74a29f
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2022-07-11 20:59:14 +0200

    Benchmark with mandelbrot.b
    1. `cargo build --release`
    2. `time target/release/brainfuck`
    
    Results:
    
    Executed in   36.60 secs    fish           external
       usr time   36.56 secs  152.00 micros   36.56 secs
       sys time    0.01 secs   82.00 micros    0.00 secs

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::io;
 use brainfuck::Interpreter;
 
 fn main() {
-    let interpreter = Interpreter::new(include_str!("./program.b"), io::stdin(), io::stdout());
-
-    if let Err(e) = interpreter.execute() {
-        panic!("error: {e}");
-    }
+    Interpreter::new(
+        include_str!("../benches/mandelbrot.b"),
+        &mut io::stdin().lock(),
+        &mut io::stdout().lock(),
+    )
+    .execute()
+    .unwrap();
 }
