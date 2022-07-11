ba6eb60bc5f296005a5a22a224c53b26e61689da
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2022-07-11 19:11:57 +0200

    Add tests for every brainfuck character

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::io::{self, Read, Write};
 
+/// The memory size that is available to a Brainfuck program.
+const DATA_SIZE: usize = 30_000;
+
 /// An intepreter that can execute Brainfuck code.
 pub struct Interpreter<'a, R, W> {
     /// Code to execute.
@@ -9,16 +12,16 @@ pub struct Interpreter<'a, R, W> {
     ip: usize,
 
     /// Zero initialized, available memory for `code`.
-    data: [u8; 30_000],
+    data: [u8; DATA_SIZE],
 
     /// Data pointer into `data`.
     dp: usize,
 
     /// Reader to read a byte from when the input instruction is encountered.
-    reader: R,
+    reader: &'a mut R,
 
     /// Writer to write a byte to when the output instruction is encountered.
-    writer: W,
+    writer: &'a mut W,
 }
 
 impl<'a, R, W> Interpreter<'a, R, W>
@@ -27,7 +30,7 @@ where
     W: Write,
 {
     /// Creates a new intepreter to execute Brainfuck code.
-    pub fn new(code: &'a str, reader: R, writer: W) -> Self {
+    pub fn new(code: &'a str, reader: &'a mut R, writer: &'a mut W) -> Self {
         Self {
             code: code.as_bytes(),
             ip: 0,
@@ -40,14 +43,17 @@ where
 
     /// Executes the program, returning an error if reading from the reader
     /// or writing to the writer fails.
-    pub fn execute(mut self) -> io::Result<()> {
+    pub fn execute(&mut self) -> io::Result<()> {
         while self.ip < self.code.len() {
             let instruction = self.code[self.ip];
             match instruction {
-                b'>' => self.dp += 1,
+                b'>' => {
+                    self.dp += 1;
+                    assert!(self.dp < DATA_SIZE);
+                }
                 b'<' => self.dp -= 1,
-                b'+' => self.data[self.dp] += 1,
-                b'-' => self.data[self.dp] -= 1,
+                b'+' => self.data[self.dp] = self.data[self.dp].wrapping_add(1),
+                b'-' => self.data[self.dp] = self.data[self.dp].wrapping_sub(1),
                 b',' => self
                     .reader
                     .read_exact(&mut self.data[self.dp..self.dp + 1])?,
@@ -90,5 +96,133 @@ where
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use crate::{Interpreter, DATA_SIZE};
+
+    #[test]
+    fn test_increment_dp() {
+        let code = ">";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        assert_eq!(interpreter.dp, 0);
+
+        interpreter.execute().unwrap();
+        assert_eq!(interpreter.dp, 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_increment_dp_overflow() {
+        // Incrementing `dp` when `dp` is already the max memory size results in an overflow
+        let code = ">".repeat(DATA_SIZE);
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        Interpreter::new(&code, &mut reader, &mut writer)
+            .execute()
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_decrement_dp_overflow() {
+        // Decrementing `dp` when `dp` is already 0 results in an overflow.
+        let code = "<";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        Interpreter::new(code, &mut reader, &mut writer)
+            .execute()
+            .unwrap();
+    }
+
+    #[test]
+    fn test_increment_byte_at_dp() {
+        let code = "+>++";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        assert_eq!(interpreter.data[0], 1);
+        assert_eq!(interpreter.data[1], 2);
+    }
+
+    #[test]
+    fn test_decrement_byte_at_dp() {
+        let code = "->--";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        // Wrapping overflow because `data` is with 0 initialized.
+        assert_eq!(interpreter.data[0], 255);
+        assert_eq!(interpreter.data[1], 254);
+    }
+
+    #[test]
+    fn test_output_byte_at_dp() {
+        let code = ".+.";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        assert_eq!(writer.get_ref()[0], 0);
+        assert_eq!(writer.get_ref()[1], 1);
+    }
+
+    #[test]
+    fn test_input_byte_at_dp() {
+        let code = ",>,>,";
+        let mut reader = Cursor::new([1, 2, 3]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        assert_eq!(interpreter.data[0], 1);
+        assert_eq!(interpreter.data[1], 2);
+        assert_eq!(interpreter.data[2], 3);
+    }
+
+    #[test]
+    fn test_loop_skip_to_back() {
+        // Execute `+` because `data[0]` is 0 then at `]` do not jump back to `+` because `data[0]`
+        // is not 0.
+        let code = "[+]";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        assert_eq!(interpreter.data[0], 0);
+    }
+
+    #[test]
+    fn test_loop_skip_to_front() {
+        // Increment `data[0]` to 2, `[` executes `.-` because it is not 0,
+        // then at `]` jump back to `.` because `data[0]` is not 0.
+        let code = "++[.-]";
+        let mut reader = Cursor::new([0]);
+        let mut writer = Cursor::new(Vec::new());
+
+        let mut interpreter = Interpreter::new(code, &mut reader, &mut writer);
+        interpreter.execute().unwrap();
+
+        assert_eq!(writer.get_ref(), &[2, 1]);
     }
 }
