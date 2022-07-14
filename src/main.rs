d3966aec4956a9ea981780e01433e8e414822f3a
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2022-07-14 17:06:10 +0200

    Test performance of the basic virtual machine
    Benchmark with mandelbrot.b
    
    1. `cargo build --release`
    2. `time target/release/brainfuck`
    
    Results:
    
    Executed in    5.34 secs    fish           external
       usr time    5.33 secs  122.00 micros    5.33 secs
       sys time    0.00 secs   49.00 micros    0.00 secs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
-use std::{io, os};
+use std::io;
+use std::time::{Duration, Instant};
 
+use brainfuck::compiler::Compiler;
 use brainfuck::interpreter::Interpreter;
+use brainfuck::virtual_machine::VirtualMachine;
 
 fn main() {
+    let program = include_str!("../programs/mandelbrot.b");
+
+    // let interpreter = measure(|| run_interpreter(program));
+    let virtual_machine = measure(|| run_virtual_machine(program));
+
+    // eprintln!("Interpreter:     {:?}", interpreter);
+    eprintln!("Virtual machine: {:?}", virtual_machine);
+}
+
+fn run_interpreter(program: &str) {
     let mut reader = io::empty();
     let mut writer = io::stdout();
 
-    Interpreter::new(
-        include_str!("../programs/hello_world.b"),
-        &mut reader,
-        &mut writer,
-    )
-    .execute()
-    .unwrap();
+    Interpreter::new(program, &mut reader, &mut writer)
+        .execute()
+        .unwrap();
+}
+
+fn run_virtual_machine(program: &str) {
+    let mut reader = io::empty();
+    let mut writer = io::stdout();
+
+    let instructions = Compiler::new(program).compile();
+
+    VirtualMachine::new(&instructions, &mut reader, &mut writer)
+        .execute()
+        .unwrap();
+}
+
+fn measure(f: impl FnOnce()) -> Duration {
+    let now = Instant::now();
+    f();
+    now.elapsed()
 }
