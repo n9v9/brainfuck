453ef5560e23da6169140b377af5c83d04f22ac6
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2022-07-14 13:35:58 +0200

    Add test programs and restructure project

---
+++ programs/hello_world.b
@@ -0,0 +1,6 @@
+[
+    Source: https://github.com/rdebath/Brainfuck/blob/master/README.md
+    Description: This program should print `Hello World!\n`.
+]
++[>[<-[]>+[>+++>[+++++++++++>][>]-[<]>-]]++++++++++<]>
+>>>>>----.<<+++.<-..+++.<-.>>>.<<.+++.------.>-.<<+.<.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,1 @@
-use std::io;
-
-use brainfuck::Interpreter;
-
-fn main() {
-    Interpreter::new(
-        include_str!("../benches/mandelbrot.b"),
-        &mut io::stdin().lock(),
-        &mut io::stdout().lock(),
-    )
-    .execute()
-    .unwrap();
-}
+fn main() {}
